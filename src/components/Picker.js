f3376b46c2c17480a9bcac2a22602f24ede8f392
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2022-01-31 17:03:57 -0300

    Update ColorPicker

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -16,15 +16,20 @@ const Picker = () => {
 				})
 			}
 		>
-			<h1>{snap.current}</h1>
-			<HexColorPickerStyle
-				color={snap.items[snap.current]}
-				onChange={(color) => (state.items[snap.current] = color)}
-			/>
-			<HexColorInputStyle
-				color={snap.items[snap.current]}
-				onChange={(color) => (state.items[snap.current] = color)}
-			/>
+			<Card>
+				<h1>{snap.current}</h1>
+				<HexColorPickerStyle
+					color={snap.items[snap.current]}
+					onChange={(color) => (state.items[snap.current] = color)}
+				/>
+				<InputWrap>
+					<p>Color Code: </p>
+					<HexColorInputStyle
+						color={snap.items[snap.current]}
+						onChange={(color) => (state.items[snap.current] = color)}
+					/>
+				</InputWrap>
+			</Card>
 		</PickerContainer>
 	);
 };
@@ -41,10 +46,33 @@ const PickerContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: center;
 
 	transition: 250ms all ease-in;
 
 	z-index: 10;
+
+	@media screen and (max-width: 768px) {
+		top: auto;
+		bottom: 12%;
+		left: 0;
+		right: 0;
+		margin-right: 0;
+		margin-left: 0;
+		width: 100%;
+	}
+`;
+
+const Card = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	padding: 25px;
+	border-radius: 14px;
+	background-color: rgba(250, 250, 250, 0.25);
+	backdrop-filter: blur(8px);
+	box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.08);
 
 	h1 {
 		font-size: 4.5em;
@@ -60,22 +88,20 @@ const PickerContainer = styled.div`
 		margin-left: -4px;
 		margin-bottom: 25px;
 
+		user-select: none;
+
 		@media screen and (max-width: 768px) {
 			width: 90%;
 			margin-left: -4px;
 			font-size: 2.5em;
 			letter-spacing: -3px;
+			margin-bottom: 15px;
 		}
 	}
 
 	@media screen and (max-width: 768px) {
-		top: 64%;
-		left: 0;
-		right: 0;
-		margin-right: 0;
-		margin-left: 0;
-
-		width: 100%;
+		width: 85%;
+		padding: 20px 10px;
 	}
 `;
 
@@ -89,15 +115,32 @@ const HexColorPickerStyle = styled(HexColorPicker)`
 	}
 `;
 
+const InputWrap = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+
+	p {
+		margin-top: 8px;
+		margin-right: 4px;
+		font-size: 14px;
+	}
+
+	@media screen and (max-width: 768px) {
+		display: none;
+	}
+`;
+
 const HexColorInputStyle = styled(HexColorInput)`
 	display: block;
 	box-sizing: border-box;
-	width: 80px;
+	width: 65px;
 	margin-top: 10px;
 	padding: 6px;
 	border: 1px solid rgba(0, 0, 0, 0);
 	border-radius: 4px;
-	background: #eee;
+	background: transparent;
 	outline: none;
 	text-transform: uppercase;
 	text-align: center;
@@ -105,9 +148,6 @@ const HexColorInputStyle = styled(HexColorInput)`
 	:focus {
 		border: 1px solid;
 		border-color: #aaa;
-	}
-
-	@media screen and (max-width: 768px) {
-		display: none;
+		background: rgba(250, 250, 250, 0.25);
 	}
 `;
